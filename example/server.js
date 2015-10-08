f1163027392c262541090d5a7cb17e1af466ee8c
Nick Mai <nick.mai@outlook.com>
2015-10-07 18:43:52 -0700

    Example server now loads method-routes as a plugin. Added two example server methods. Added utility module with a depth test function

--- example/server.js
+++ example/server.js
@@ -1,3 +1,47 @@
 //Nick Mai
 //example server- will implement the 'add' method in the gist example
 
+var Hapi = require('hapi');
+
+//Configure the server
+var server = new Hapi.Server();
+server.connection({ port: 8080 });
+
+
+//Display method usage on the root path
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('<h2>hapi-method-routes demo</h2>Usage: /methods/your_method/opt_arg1,opt_arg2,...');
+    }
+});
+
+
+//Load all necessary plugins (in this case, just one)
+server.register([require('..')], {}, function (err) {
+
+	//Throw any errors
+    if (err) {
+        console.error('Failed to load a plugin:', err);
+    }
+
+    //Create a method that returns 'Hello World!'
+    server.method('hello', 
+    	function (next) {
+	    	next(null, 'Hello World!');
+	    }, {});
+
+    //Create a method that adds 2 numbers
+    var add = function (x, y, next) {
+	    next(null, x + y);
+	};
+    server.method('math.add', add, {});
+
+
+    //Start the server
+    server.start(function () {
+	    console.log('Server running at:', server.info.uri);
+	});
+
+});
